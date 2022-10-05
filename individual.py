7757d4246ff7831d8cf09375eb43af2f5705f9a3
eMoushen <strigalov1991@mail.ru>
2022-10-06 01:03:34 +0300

    fix

--- individual.py
+++ individual.py
@@ -1,25 +1,83 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
 
 if __name__ == '__main__':
-    list_of_dicts = list()
+    products = []
+    while True:
+        print("Напишите Help для справк по программе")
+        print("введите комаду")
+        command = input()
 
-    for i in range(3):
-        my_dict = dict()
+        if command == "Help" or command == "help" or command == "HELP":
+            print("Список команд:\n")
+            print("[add - добавить товар]")
+            print("[list - вывести список товаров]")
+            print("[select - товары в наличии]")
+            print("[help - отобразить справку]")
+            print("[exit - завершить работу с программой]")
 
-        my_dict['Название товара'] = input("Введите название товара: ")
-        my_dict['Название магазина'] = input("Введите название магазина: ")
-        my_dict['Стоимость товаров в руб'] = int(input("Введите стоимость: "))
+        elif command == "add":
+            print("сколько добавить товаров?")
+            kol = int(input())
+            k = 0
+            for m in range(kol):
+                k = k + 1
+                name = input("Введите название для "+"{}".format(k)+" товара:  ")
+                shope = input("Введите название магазина:  ")
+                price = int(input("Стоимость товара:  "))
+                product = {
+                    'name': name,
+                    'price': price,
+                    'shope': shope,
+                }
+                products.append(product)
+                products.sort(key=lambda item: item.get('name', ''))
 
-        list_of_dicts.append(my_dict)
+        elif command == 'list':
+            # Заголовок таблицы.
+            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+                '-' * 5,
+                '-' * 20,
+                '-' * 14,
+                '-' * 17
+            )
+            print(line)
+            print(
+                '| {:^5} | {:^20} | {:^14} | {:^17} |'.format(
+                    "№",
+                    "Название товара",
+                    "Цена",
+                    "Название магазина"
+                )
+            )
+            print(line)
+            # Вывести данные о всех товарах.
+            for idx, product in enumerate(products, 1):
+                print(
+                    '| {:>5} | {:<20} | {:<14} | {:>17} |'.format(
+                        idx,
+                        product.get('name', ''),
+                        product.get('price', 0),
+                        product.get('shope', '')
+                    )
+                )
+            print(line)
 
-    list_of_dicts = sorted(list_of_dicts, key=lambda x: x["Название товара"])
-    is_find = False
+        elif command == 'exit':
+            break
 
-    for item in list_of_dicts:
-        if item['Название товара'].__contains__('New balance'):
-            print(f'\nТовар в наличии: {item["Название товара"]}\nЦена: {item["Стоимость товаров в руб"]}\n\n')
-            is_find = True
+        elif command == 'select':
+            # Проверить наличие товара.
+            nalich = "new balance"
 
-    if not is_find:
-        print("Таких товаров нет!")
+            flag = False
+            for product in products:
+                if nalich in product['name']:
+                    print(f'Товар в наличии: {product["name"]}\nЦена: {product["price"]}\n')
+                    flag = True
+
+            if not flag:
+                print(f'\nТаких товаров нет: {nalich}')
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
